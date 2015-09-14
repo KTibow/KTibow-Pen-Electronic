b99ef0938b2b120e1b99bb490dd545d8e41d1129
kendell <kendell.r@outlook.com>
2015-09-14 06:57:05 -0700

    Update Arduino1.ino

--- Bluetooth/Arduino1.ino
+++ Bluetooth/Arduino1.ino
@@ -1,4 +1,24 @@
 class Bluetooth
 {
+  public:
+    #include "Bluetooth.h"
+    class BluetoothConnector
+    {
+      public:
+      
+    }
+    void MakeBluetoothConnector(String Name) {
+      BluetoothConnector Name();
+    }
+  private:
+    
+  
+}
 
+void setup() {
+  Bluetooth PenBluetooth();
+}
+
+void loop() {
+  
 }
